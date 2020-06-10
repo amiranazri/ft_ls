eaa4f9147f4aa8f6ca8ceb12728d9a62a09e582f
Amira Nazri <anazri@hotmail.com>
2020-06-10 22:09:10 +0200

    Add files via upload
    Created function for leaks

--- free.c
+++ free.c
@@ -1,26 +1,13 @@
 #include "ft_ls.h"
 
-t_list *free_list(t_list *head) 
-{ 
-    t_list *tmp;
-    char *file_name;
-
-    tmp = head;
-    while (head != NULL)
+void    free_list(t_list    *list)
+{
+    t_list  *tmp;
+    tmp = list;
+    while(tmp != NULL) 
     {
-        if (head == NULL)
-        {
-            tmp = (t_list *)malloc(sizeof(t_list));
-            tmp->data_name = ft_strdup(file_name);
-            tmp->next = head;
-            head = tmp;
-        }
-        else
-        {
-            insert_node(file_name, tmp);
-            //pass head the new node address
-            head = tmp;
-        }
+        tmp = tmp->next;
+        free (list);
+        list = tmp;
     }
-    return (head);
 }
